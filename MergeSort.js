3c8f5347d9008e4efacbd771d8a4052c5fb4e6ce
Shofiqur Rahman <shofi384@gmail.com>
2018-08-09 11:37:06 -0400

    Update MergeSort.js
    Comments added

--- MergeSort.js
+++ MergeSort.js
@@ -8,19 +8,19 @@ let main = () => {
 let mergeSort = (array, temp, low, high) => {
     if(low < high) {
         let mid = Math.floor((low+high) / 2);
-        mergeSort(array, temp, low, mid);
-        mergeSort(array, temp, mid+1, high);
-        merge(array, temp, low, mid, high);
+        mergeSort(array, temp, low, mid);   // sort left half
+        mergeSort(array, temp, mid+1, high);    //sort right half
+        merge(array, temp, low, mid, high); // merge the both half together sorted
     }
 }
 
 let merge = (array, temp, low, mid, high) => {
-    for(let i=low; i<=high; i++) {
+    for(let i=low; i<=high; i++) {  // copy the elementes to be merged in a temporary array
         temp[i] = array[i];
     }
     
     let current=low, left=low, right=mid+1;
-    while(left<=mid && right<=high) {
+    while(left<=mid && right<=high) {   // compare left and right and add them accordingly
         if(temp[left] <= temp[right]) {
             array[current++] = temp[left++];
         }
@@ -29,7 +29,7 @@ let merge = (array, temp, low, mid, high) => {
         }
     }
     
-    let remaining = mid - left + 1;
+    let remaining = mid - left + 1; // copy the rest of the left half
     for(let i=0; i<remaining; i++) {
         array[current++] = temp[left+i];
     }
